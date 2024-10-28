e03c65d6ba083028fb36605db06ea407f7e614fa
notebook <kkang56508@naver.com>
2024-10-28 12:18:36 +0900

    chore: Complete deployment environment setup

--- src/main/java/com/PhysicalTrack/PhysicalTrackApplication.java
+++ src/main/java/com/PhysicalTrack/PhysicalTrackApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PhysicalTrackApplication {
+public class PhysicalTrackApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PhysicalTrackApplication.class, args);
